8f35c0ac2a8c56ee9dd1c2ff99c2d257babbb81a
Hussein891 <hussinhadid@gmail.com>
2025-05-14 21:20:26 +0200

    create styles and add title to home view

---
+++ lib/features/home/data/presentation/view/widget/featured_list_view.dart
@@ -0,0 +1,20 @@
+import 'package:books_app/features/home/data/presentation/view/widget/custom_lsit_view_item.dart';
+import 'package:flutter/cupertino.dart';
+
+class FeaturdBooksListView extends StatelessWidget {
+  const FeaturdBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .25,
+      child: ListView.builder(
+        itemCount: 8,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return FeaturedListViewItem();
+        },
+      ),
+    );
+  }
+}
--- lib/features/home/data/presentation/view/widget/home_view_body.dart
+++ lib/features/home/data/presentation/view/widget/home_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:books_app/core/uitel/styles.dart';
 import 'package:books_app/features/home/data/presentation/view/widget/custom_app_bar.dart';
 import 'package:books_app/features/home/data/presentation/view/widget/custom_lsit_view_item.dart';
+import 'package:books_app/features/home/data/presentation/view/widget/featured_list_view.dart';
 
 import 'package:flutter/material.dart';
 
@@ -8,23 +10,14 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [const CustomAppBar(), FeaturdBooksListView()]);
-  }
-}
-
-class FeaturdBooksListView extends StatelessWidget {
-  const FeaturdBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .25,
-      child: ListView.builder(
-        itemCount: 8,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return FeaturedListViewItem();
-        },
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          const CustomAppBar(),
+          FeaturdBooksListView(),
+          Text('Bist Seller', style: Styles.titleMedium),
+        ],
       ),
     );
   }
