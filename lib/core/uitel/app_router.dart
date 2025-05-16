35bedb5e3330b84f1d0fc40a308622261196f1bd
Hussein891 <hussinhadid@gmail.com>
2025-05-16 20:44:41 +0200

    create custom book deteils  app bar

--- lib/core/uitel/app_router.dart
+++ lib/core/uitel/app_router.dart
@@ -1,13 +1,19 @@
+import 'package:books_app/features/home/data/presentation/view/book_details_view.dart';
 import 'package:books_app/features/home/data/presentation/view/home_view.dart';
 import 'package:books_app/features/splash/presentation/views/splash.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeVeiw = '/homeView';
+  static const kDetailsVeiw = '/detailsView';
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => const SplashView()),
       GoRoute(path: kHomeVeiw, builder: (context, state) => const HomeView()),
+      GoRoute(
+        path: kDetailsVeiw,
+        builder: (context, state) => const BookDetailsView(),
+      ),
     ],
   );
 }
