19caaf05dc7aed629c369b8ea56527571dc734ad
Hussein891 <hussinhadid@gmail.com>
2025-05-17 20:21:55 +0200

    create custom search text field

--- lib/features/search/presntation/view/widget/search_view_body.dart
+++ lib/features/search/presntation/view/widget/search_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:books_app/features/search/presntation/view/widget/custom_searche_text_field.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -8,27 +9,6 @@ class SearchViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(children: [CustomSearchTextField()]),
-    );
-  }
-}
-
-class CustomSearchTextField extends StatelessWidget {
-  const CustomSearchTextField({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.white),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        hintText: 'Search',
-        suffixIcon: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.search, size: 24),
-        ),
-      ),
     );
   }
 }
