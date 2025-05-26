ac648820e70b2862015066cfd5245b39291b6d72
Hussein891 <hussinhadid@gmail.com>
2025-05-26 17:48:55 +0200

    create servcer failure from dio error

--- lib/core/errors/falilures.dart
+++ lib/core/errors/falilures.dart
@@ -1,7 +1,56 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMassage;
+  const Failure(this.errMassage);
+}
 
+class ServerFailure extends Failure {
+  ServerFailure(super.errMassage);
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('SendT Imeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServerFailure(
+          'You Have لآadCertificatet with ApiServer, Please try later!',
+        );
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromBadResponse(
+          dioError.response!.statusCode!,
+          dioError.response!.data!,
+        );
 
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to ApiServer was Cancel');
+      case DioExceptionType.connectionError:
+        return ServerFailure('You Have Connection Error, Please try later!');
+      case DioExceptionType.unknown:
+        if (dioError.message!.contains('SocketException')) {
+          return ServerFailure('No Internet Connection');
+        }
+        return ServerFailure('Unexpected Eroor , Please try later!');
+      // ignore: unreachable_switch_default
+      default:
+        return ServerFailure('Opps There Was an Error ,Please try later!');
+    }
+  }
+
+  factory ServerFailure.fromBadResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your Request not found , Please try later!');
+    } else if (statusCode == 500) {
+      return ServerFailure('Interal Server error , Please try later!');
+    } else {
+      return ServerFailure('Opps There Was an Error ,Please try later!');
+    }
+  }
+}
 
 //https://www.googleapis.com/books/v1/volumes?q=programming
--- lib/features/home/data/repo/home_repo_impl.dart
+++ lib/features/home/data/repo/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:books_app/core/uitel/api_service.dart';
 import 'package:books_app/features/home/data/model/book_model/book_model.dart';
 import 'package:books_app/features/home/data/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl extends HomeRepo {
   final ApiService apiService;
@@ -20,7 +21,10 @@ class HomeRepoImpl extends HomeRepo {
       }
       return right(book);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
